ec6f7c9374e853f9ccdd7746bb94d164f9199d48
anugraha4 <96745531+anugraha4@users.noreply.github.com>
2022-12-14 19:57:36 +0530

    NestAppBackend

---
+++ NestDigitalApplication/src/main/java/com/example/NestDigitalApplication/NestDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.NestDigitalApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestDigitalApplication.class, args);
+	}
+
+}
