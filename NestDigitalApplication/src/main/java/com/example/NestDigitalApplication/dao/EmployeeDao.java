6f30ddfc9bf68162637e98c2c4775f4a56af508f
anugraha4 <96745531+anugraha4@users.noreply.github.com>
2022-12-15 20:54:25 +0530

    third commit

--- NestDigitalApplication/src/main/java/com/example/NestDigitalApplication/dao/EmployeeDao.java
+++ NestDigitalApplication/src/main/java/com/example/NestDigitalApplication/dao/EmployeeDao.java
@@ -1,4 +1,15 @@
 package com.example.NestDigitalApplication.dao;
 
-public interface EmployeeDao {
+import com.example.NestDigitalApplication.model.Employee;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface EmployeeDao extends CrudRepository<Employee,Integer> {
+
+
+    @Query(value = "SELECT `id`, `designation`, `email`, `emp_code`, `name`, `password`, `phone`, `username` FROM `employee` WHERE `name` LIKE %:name%", nativeQuery = true)
+    List<Employee> SearchEmployee(@Param("name") String name);
 }
