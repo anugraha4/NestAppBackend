903faa339d431e8d93952e904484abd720899581
anugraha4 <96745531+anugraha4@users.noreply.github.com>
2022-12-15 21:02:44 +0530

    fourth commit

--- NestDigitalApplication/src/main/java/com/example/NestDigitalApplication/dao/EmployeeDao.java
+++ NestDigitalApplication/src/main/java/com/example/NestDigitalApplication/dao/EmployeeDao.java
@@ -1,6 +1,8 @@
 package com.example.NestDigitalApplication.dao;
 
 import com.example.NestDigitalApplication.model.Employee;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +14,10 @@ public interface EmployeeDao extends CrudRepository<Employee,Integer> {
 
     @Query(value = "SELECT `id`, `designation`, `email`, `emp_code`, `name`, `password`, `phone`, `username` FROM `employee` WHERE `name` LIKE %:name%", nativeQuery = true)
     List<Employee> SearchEmployee(@Param("name") String name);
+
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE `employee` SET `designation`= :designation,`email`= :email,`emp_code`= :emp_code,`name`= :name,`password`= :password,`phone`= :phone,`username`= :username WHERE `id`= :id", nativeQuery = true)
+    void EditEmployee(@Param("id") int id, @Param("designation") String designation,@Param("email") String email,@Param("emp_code") String emp_code,@Param("name") String name,@Param("password") String password,@Param("phone") String phone,@Param("username") String username);
 }
